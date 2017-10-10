5a0c0d0841bf6ceb2ead49262d6cc8a9ed2fa34a
yangyan <277160299@qq.com>
2017-10-10 15:51:59 +0800

    任务节点设置用户的地方增加删除用户的功能

--- src/views/zwfw/hall/CompositeWindowHistory.vue
+++ src/views/zwfw/hall/CompositeWindowHistory.vue
@@ -212,7 +212,7 @@
                                             <span v-if="file.url!=null && file.url!=''">
                                             <a target="_blank"
                                                v-if="file.fileType == 'doc' || file.fileType == 'docx' || file.fileType == 'xls' || file.fileType == 'xlsx' || file.fileType == 'ppt'"
-                                               :href="'https://view.officeapps.live.com/op/view.aspx?src=' + win.cxt + file.url ">[{{index + 1}}]</a>
+                                               :href="'https://view.officeapps.live.com/op/view.aspx?src=' + file.url ">[{{index + 1}}]</a>
                                             <a v-else :href="file.url"
                                                target="_blank">[{{index + 1}}]</a>
                                             </span>
