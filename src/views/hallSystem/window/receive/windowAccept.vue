c7e6f3d57bcda134db74520a1df50c5a115b26d3
yangyan <277160299@qq.com>
2017-12-15 20:22:55 +0800

    收件页面修改

--- src/views/hallSystem/window/receive/windowAccept.vue
+++ src/views/hallSystem/window/receive/windowAccept.vue
@@ -381,14 +381,11 @@
                                         width="300">
                                     <template scope="scope">
                                         {{scope.row.name}}
-                                        <div v-if="scope.row.multipleFile">
+                                        <div v-if="scope.row.multipleFile" style="color:blue">
                                             预审资料：
                                             <span v-for="(file,index) in scope.row.multipleFile">
                                             <span v-if="file.url!=null && file.url!=''">
-                                            <a target="_blank"
-                                               v-if="file.fileType == 'doc' || file.fileType == 'docx' || file.fileType == 'xls' || file.fileType == 'xlsx' || file.fileType == 'ppt'"
-                                               :href="'https://view.officeapps.live.com/op/view.aspx?src=' + file.url ">[{{index + 1}}]</a>
-                                            <a v-else :href="file.url"
+                                            <a  :href="file.url"
                                                target="_blank">[{{index + 1}}]</a>
                                             </span>
                                             <span v-else>未上传</span>
