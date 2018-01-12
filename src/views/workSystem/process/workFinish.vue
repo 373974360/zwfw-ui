ba76681c9e5d778903921adc64c4b80d9ae7de89
yangyan <277160299@qq.com>
2018-01-12 18:19:19 +0800

    打印的受理单，一次性告知单都用 html 形式，修复预审界面中的变量名不符合驼峰命名法的地方

--- src/views/workSystem/process/workFinish.vue
+++ src/views/workSystem/process/workFinish.vue
@@ -536,7 +536,9 @@
             },
             print_ycxgzd(processNumber) {
                 if (processNumber != null) {
-                    window.open('/api/hallSystem/hallCompositeWindow/downloadYcxgzd?processNumber=' + processNumber);
+                    // window.open('/api/hallSystem/hallCompositeWindow/downloadYcxgzd?processNumber=' + processNumber);
+                    window.open('/admin/print/ycxgzd.html?processNumber=' + processNumber);
+
                 }
             },
             handlePictureCardPreview(file) {
--- src/views/workSystem/process/workPending.vue
+++ src/views/workSystem/process/workPending.vue
@@ -773,8 +773,8 @@
             },
             print_ycxgzd(processNumber) {
                 if (processNumber != null) {
-                    // window.open('/admin/print/ycxgzd.html?processNumber=' + processNumber);
-                    window.open('/api/hallSystem/hallCompositeWindow/downloadYcxgzd?processNumber=' + processNumber);
+                    window.open('/admin/print/ycxgzd.html?processNumber=' + processNumber);
+                    // window.open('/api/hallSystem/hallCompositeWindow/downloadYcxgzd?processNumber=' + processNumber);
                 }
             },
 
