171392a37d52585d2271eace3e2abb9d6ec260f9
yangyan <277160299@qq.com>
2018-01-25 14:59:46 +0800

    界面自动填充优化、输入框、操作按钮、确认收件按钮禁用提示优化

--- src/views/hallSystem/window/receive/windowAccept.vue
+++ src/views/hallSystem/window/receive/windowAccept.vue
@@ -151,7 +151,7 @@
                                                         </el-row>
                                                         <el-row :gutter="10">
                                                             <el-col :span="12">
-                                                                <el-tooltip content="如果只有15位注册号，使用000作为前缀后面跟入15位注册号"
+                                                                <el-tooltip content="如果只有15位注册号，使用000作为前缀后面跟入15位注册号，可用于注册查询"
                                                                             placement="bottom"
                                                                             effect="light">
                                                                     <el-input v-model="companyCode"
@@ -177,7 +177,7 @@
                                                                         effect="light">
                                                                     <el-button type="primary"
                                                                                @click="checkLegalMemberExist()"
-                                                                               :disabled="!companyCode">注册查询
+                                                                               :disabled="!companyCode && !memberCode">注册查询
                                                                     </el-button>
                                                                 </el-tooltip>
                                                             </el-col>
@@ -201,7 +201,7 @@
                                                                 </el-input>
                                                             </el-col>
                                                             <el-col :span="10">
-                                                                <el-tooltip content="政务网站使用此身份证号码作为认证账号用户名登录"
+                                                                <el-tooltip content="政务网站使用此身份证号码作为认证账号用户名登录，可用于注册查询"
                                                                             placement="bottom"
                                                                             effect="light">
                                                                     <el-input v-model="memberCode" placeholder="法人身份证号"
@@ -354,7 +354,7 @@
                                                     v-model="selectedItem"
                                                     placeholder="选择分类下的事项"
                                                     filterable
-                                                    @change="changeItem" style="width:100%">
+                                                    @change="changeItem" :loading="loadingItem" style="width:100%" >
                                                 <el-option
                                                         v-for="item in optionsName"
                                                         :key="item.id"
@@ -1523,13 +1523,14 @@
              * 检测法人用户是否注册，如果注册，返回用户信息，如果没有注册显示出快速注册界面
              * */
             checkLegalMemberExist() {
-                if (!checkSocialCreditCode(this.companyCode)) {
+                if (this.companyCode && !checkSocialCreditCode(this.companyCode)) {
                     this.companyInfo = {};
                     this.$message.warning("社会统一信用代码不正确，请重新输入");
                     return;
                 }
                 checkLegalMemberExist({
-                    companyCode: this.companyCode
+                    companyCode: this.companyCode,
+                    memberCode:this.memberCode
                 }).then(response => {
                     if (response.httpCode === 200) {
                         if (response.data == null) {
