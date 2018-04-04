4186a2d60a7ddafdffa9cb18ad65bfecb034a28d
songshanshan <769172685@qq.com>
2018-04-04 10:54:35 +0800

    数据可视化大屏

--- src/views/hallSystem/visualization/dashboard.js
+++
@@ -1,9 +0,0 @@
-import {gridLayout} from '../../../components/GridLayout/grid_layout';
-import {gridItem} from '../../../components/GridLayout/grid_item';
-import {LayoutManager} from '../../../components/GridLayout/layout_manager';
-
-export {
-    gridLayout,
-    gridItem,
-    LayoutManager
-}
