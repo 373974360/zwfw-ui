30e9eeacb2d35a918776c35a23264de5baf38ee7
yangyan <277160299@qq.com>
2018-05-31 13:25:36 +0800

    材料表单的表单域配置表格中增加显示 key，方便在 word 模板中配置变量名

--- src/views/zwfwSystem/business/itemPretrialForm.vue
+++ src/views/zwfwSystem/business/itemPretrialForm.vue
@@ -56,7 +56,7 @@
             </el-table-column>
             <el-table-column
                     prop="labelAlias"
-                    label="别名"
+                    label="此表单中的别名"
                     width="180">
                 <template slot-scope="scope">
                     <el-input :disabled="!scope.row.fieldId" v-model="scope.row.labelAlias"
