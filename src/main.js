58bf9856a087aa65bf0d9a4ad048ddcac9cf9b20
songshanshan <769172685@qq.com>
2018-06-26 13:31:32 +0800

    大屏事项办结率综合窗口暂不显示，百度地图显示

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import BaiduMap from 'vue-baidu-map';
+// import BaiduMap from 'vue-baidu-map';
 import App from './App';
 import router from './router';
 import store from './store';
@@ -36,10 +36,10 @@ Vue.component('multiselect', Multiselect);
 Vue.component('Sticky', Sticky);
 Vue.use(ElementUI);
 Vue.use(vueWaves);
-Vue.use(BaiduMap, {
-    // ak 是在百度地图开发者平台申请的密钥 详见 http://lbsyun.baidu.com/apiconsole/key */
-    ak: 'kiBDeRDVT4UsjzkEj7SlWBVKcZT2u1zG'
-})
+// Vue.use(BaiduMap, {
+//     // ak 是在百度地图开发者平台申请的密钥 详见 http://lbsyun.baidu.com/apiconsole/key */
+//     ak: 'QD1dS3eRuLmQOXX0HSz6xgef8Dr8zh75'
+// })
 moment.locale('zh-cn');
 
 // register global utility filters.
